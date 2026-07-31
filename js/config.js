b0daa1a9493b2ad46c67a7c46cbcbcf66a5326e3
FRAME-890 <framef590@gmail.com>
2026-07-31 11:48:13 +0700

    Replace ADMIN_INVITE_CODE with FUNCTIONS_URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 export const SUPABASE_URL = "https://orfurfhwcbzihacgwlva.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_sEDz9yhZx8SuwypjSwmP1w_OvTo8HQS";
-export const ADMIN_INVITE_CODE = "STARLIVE095345";
+export const FUNCTIONS_URL = `${SUPABASE_URL}/functions/v1`;
